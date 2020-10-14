9b071363adffbdd82261e7009700d8cb122b5593
eugenemrobert <eugenemuloma40@gmail.com>
2020-10-14 13:22:41 +0300

    Initial commit

---
+++ collatz2.py
@@ -0,0 +1,20 @@
+def collatz(number):
+
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
+        return result
+
+while True:
+    n = input("Give me a number: ")
+    try:
+        while n != 1:
+            n = collatz(int(n))
+    except ValueError:
+        print('ValueError: You must enter an integer')
+    if n == '':
+        break
